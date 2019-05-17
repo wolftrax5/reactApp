587a8304d27affe66cc58113f2466c5cb763f877
Alejandro Medina <5almero5@gmail.com>
2019-05-17 13:11:27 -0500

    modular coponetns asyng/await

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,4 +1,5 @@
 // base code of webpack to multiple configs
+const path = require('path');
 const webpack = require('webpack');
 const merge = require("webpack-merge");
 
@@ -6,10 +7,13 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const APP_DIR = path.resolve(__dirname, '../src'); 
+
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
       {
+        entry: ['@babel/polyfill', APP_DIR],
         module: {
           rules: [
             {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './mystyless.scss';
+import App from './containers/App'
 
-const App = () => {
-  return (
-    <div className='app'>
-    <img 
-   alt="worlftrax-green"
-   src="/dist/images/wolftrax-green.jpg" className="app-header" 
-/>
-      <p>
-        We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
-        have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
-        to prove it.
-      </p>
-      <p>- Captain Kirk</p>
-    </div>
-  );
-};
 
 console.log('process.env.VERSION', process.env.VERSION); // custom
 console.log('process.env.PLATFORM', process.env.PLATFORM); // custm
