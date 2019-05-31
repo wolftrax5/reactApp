67d1550d45451ae48ea6ff3cf0f60c494665348c
Alejandro Medina <5almero5@gmail.com>
2019-05-30 20:02:59 -0500

    add PropTypes

--- src/UI/Foo/Foo.test.js
+++ src/UI/Foo/Foo.test.js
@@ -4,16 +4,16 @@ import { shallow } from 'enzyme';
 import Foo from './index';
 
 const setUp = () => {
-    const props = {
-        text: 'some to comment',
-      };
-      const wrapper = shallow(<Foo />);
-      return { wrapper, props };
-}
+  const props = {
+    text: 'some to comment',
+  };
+  const wrapper = shallow(<Foo />);
+  return { wrapper, props };
+};
 
 describe('Foo Test Suite', () => {
-    it('Should have an text', () => {
-      const { wrapper } = setUp();
-      expect(wrapper.find('p').exists()).toBe(true);
-    });
+  it('Should have an text', () => {
+    const { wrapper } = setUp();
+    expect(wrapper.find('p').exists()).toBe(true);
   });
+});
--- src/UI/Foo/index.js
+++ src/UI/Foo/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Foo = ({ text }) => (
   <div>
     <p>I am Foo! Pleasure to meet you. {text}</p>
   </div>
 );
-
+Foo.propTypes = {
+  text: PropTypes.string,
+};
 export default Foo;
