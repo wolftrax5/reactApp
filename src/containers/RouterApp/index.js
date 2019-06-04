382cf8f0d708afc53a5f62e6e7e859f41eb1e0da
Alejandro Medina <5almero5@gmail.com>
2019-06-04 18:50:43 -0500

    Add style and core grid template

--- src/containers/RouterApp/index.js
+++ src/containers/RouterApp/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+import './RouterApp.scss';
 import App from '../App';
 
 const About = () => <h1>ABOUT</h1>;
@@ -13,13 +14,21 @@ const Links = () => (
 );
 const RouterApp = () => (
   <Router>
-    <div>
-      <Links />
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/about" component={About} />
-        <Route render={() => <h1>Page Not Found</h1>} />
-      </Switch>
+    <div className="core-container ">
+      <header id="core-header">
+        <h1>My APP</h1>
+      </header>
+      <aside id="core-aside">
+        <Links />
+      </aside>
+      <section id="core-section">
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/about" component={About} />
+          <Route render={() => <h1>Page Not Found</h1>} />
+        </Switch>
+      </section>
+      <footer id="core-footer">Footer</footer>
     </div>
   </Router>
 );
