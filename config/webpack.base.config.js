7c7bf803e3a440bc11651d4879b233bd90e42379
Alejandro Medina <5almero5@gmail.com>
2020-07-09 17:51:54 -0500

    update build run script

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,76 +1,78 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // base code of webpack to multiple configs
 // the estlint come from https://github.com/wesbos/eslint-config-wesbos
 const path = require('path');
 const webpack = require('webpack');
-const merge = require("webpack-merge");
+const merge = require('webpack-merge');
 
-const CopyWebpackPlugin = require('copy-webpack-plugin'); 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const APP_DIR = path.resolve(__dirname, '../src'); 
+const APP_DIR = path.resolve(__dirname, '../src');
 
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
-      {
-        entry: ['@babel/polyfill', APP_DIR],
-        output: {
-            path: path.resolve(__dirname, '../dist'),
-          },
-        module: {
-          rules: [
-            {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader'
-              }
-            },
-            {
-              test: /\.(s*)css$/,
-              use: [
-                {
-                  loader: PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                },
-                'css-loader',
-                'sass-loader'
-              ]
-            },
-            // -- Assets
-            {
-              test: /\.(png|svg|jpg|gif)$/,
-              use: ['file-loader']
-            }
-          ]
-        },
-        resolve: {
-        alias: {
-          "@": path.resolve(__dirname, '../src'),
-          "@assets": path.resolve(__dirname, '../src/static/assets'),
-          "@utils": path.resolve(__dirname, '../src/Utils/'),
-          "@ui": path.resolve(__dirname, '../src/UI/'),
-          "@containers": path.resolve(__dirname, '../src/containers/'),
-        }
+    {
+      entry: ['@babel/polyfill', APP_DIR],
+      output: {
+        path: path.resolve(__dirname, '../dist'),
       },
-        plugins: [
-          new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-          }),
-          new webpack.DefinePlugin({ 
-            'process.env.VERSION': JSON.stringify(env.VERSION),
-            'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
-          }),
-          new CopyWebpackPlugin({
-            patterns: [ { from: 'src/static/assets', to: '../dist/assets'} ]
-          }),
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
+            test: /\.(s*)css$/,
+            use: [
+              {
+                loader: PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              },
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          // -- Assets
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            loader: 'file-loader',
+            options: {
+              name: 'assets/images/[name].[ext]',
+            },
+          },
         ],
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+          '@assets': path.resolve(__dirname, '../src/static/assets'),
+          '@utils': path.resolve(__dirname, '../src/Utils/'),
+          '@ui': path.resolve(__dirname, '../src/UI/'),
+          '@containers': path.resolve(__dirname, '../src/containers/'),
+        },
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: './index.html',
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
+        }),
+        new CopyWebpackPlugin({
+          patterns: [{ from: 'src/static/assets', to: '../dist/assets' }],
+        }),
+      ],
       devServer: {
         port: 9001,
-        contentBase: APP_DIR,
-        watchContentBase: true,
-      }
-    }
-  ])
+      },
+    },
+  ]);
 };
--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,8 +1,8 @@
 // email valitator
-export const isEmail = (email) => {
-    if (!email.match(/^[a-z0-9_]+\+?[a-z0-9]*@[a-z0-9_]+\.[a-z]{2,6}$/i)) {
-      // console.error(`${email} Is not a valid e-mail`);
-      return false;
-    }
-    return true;
-  };
+export const isEmail = email => {
+  if (!email.match(/^[a-z0-9_]+\+?[a-z0-9]*@[a-z0-9_]+\.[a-z]{2,6}$/i)) {
+    // console.error(`${email} Is not a valid e-mail`);
+    return false;
+  }
+  return true;
+};
